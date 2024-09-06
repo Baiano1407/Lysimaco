170acdb5b851e561df36b9755a111320953598ec
Baiano1407 <127527442+Baiano1407@users.noreply.github.com>
2024-09-06 20:54:34 -0300

    a

--- script.js
+++ script.js
@@ -6,28 +6,28 @@ const textoResultado = document.querySelector(".texto-resultado");
 
 const perguntas = [
     {
-        enunciado: "Assim que saiu da escola você se depara com uma nova tecnologia, um chat que consegue responder todas as dúvidas que uma pessoa pode ter, ele também gera imagens e áudios hiper-realistas. Qual o primeiro pensamento?",
+        enunciado: "Você pode realizar todos os sonhos que almeja! Basta acreditar em si mesmo.",
         alternativas: [
             {
-                texto: "Isso é assustador!",
-                afirmacao: "No início ficou com medo do que essa tecnologia pode fazer. "
+                texto: "Isso é incrível!",
+                afirmacao: "Agradeço pelas palavras de apoio."
             },
             {
-                texto: "Isso é maravilhoso!",
-                afirmacao: "Quis saber como usar IA no seu dia a dia."
+                texto: "Talvez não consigo...",
+                afirmacao: "Fico inseguro por talvez não conseguir realizar."
             }
         ]
     },
     {
-        enunciado: "Com a descoberta desta tecnologia, chamada Inteligência Artificial, uma professora de tecnologia da escola decidiu fazer uma sequência de aulas sobre esta tecnologia. No fim de uma aula ela pede que você escreva um trabalho sobre o uso de IA em sala de aula. Qual atitude você toma?",
+        enunciado: "Todas as pessoas tem potencial para realizar tudo que quiser. Após ouvir essas palavras, você encontra uma pessoa triste e desanimada na rua. O que você faz?",
         alternativas: [
             {
-                texto: "Utiliza uma ferramenta de busca na internet que utiliza IA para que ela ajude a encontrar informações relevantes para o trabalho e explique numa linguagem que facilite o entendimento.",
-                afirmacao: "Conseguiu utilizar a IA para buscar informações úteis."
+                texto: "Conversa com essa pessoa e lhe dá motivação para seguir na vida, após uma longa conversa.",
+                afirmacao: "Se sente feliz por ter ajudado aquela pessoa"
             },
             {
-                texto: "Escreve o trabalho com base nas conversas que teve com colegas, algumas pesquisas na internet e conhecimentos próprios sobre o tema.",
-                afirmacao: "Sentiu mais facilidade em utilizar seus próprios recursos para escrever seu trabalho."
+                texto: "Passa como se não tivesse visto nada e segue sua caminhada para casa.",
+                afirmacao: "Se sentiu triste e começou a refletir sobre seu erro depois."
             }
         ]
     },
